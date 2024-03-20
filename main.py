e5ae5a0e744dc26988e3f68c673bcc931d785864
Lesalon_Steve... <lesalonsteven@gmail.com>
2024-03-20 14:50:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+from question_model import Question
+# from data import question_data
+from quiz_brain import QuizBrain
+
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
+url = "https://opentdb.com/api.php"
+response = requests.get(url=url, params=parameters)
+response.raise_for_status()
+question_data = response.json()["results"]
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#!/bin/bash
+
+
+git add .
+
+# Commit changes
+git commit -m "Automated commit"
+
+# Push changes to the remote repository
+git push origin main
